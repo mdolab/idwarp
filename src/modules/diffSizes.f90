071dd0e8cf087381e4ccbc0458c6edadd6b11861
kenway <kenway@localhost>
2014-10-05 10:34:27 -0400

    Added several missing files.

---
+++ src/modules/diffSizes.f90
@@ -0,0 +1,14 @@
+module diffSizes
+  use precision
+  save
+
+  integer(kind=intType) :: isize1ofdrfnormals
+  integer(kind=intType) :: isize2ofdrfnormals
+
+  integer(kind=intType) :: isize1ofdrfnormals0
+  integer(kind=intType) :: isize2ofdrfnormals0
+
+  integer(kind=intType) :: isize1ofdrfxu
+  integer(kind=intType) :: isize2ofdrfxu
+
+end module diffSizes
