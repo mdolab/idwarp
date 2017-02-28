ec542f2e98d500e80b20c8c8341730db933e6e0a
John Jasa <johnjasa11@gmail.com>
2017-02-28 14:43:25 -0500

    Added a simple script to check dot-product of warp derivs on the Python level

---
+++ python/examples/structured/dotprod.py
@@ -0,0 +1,53 @@
+# this is a simple script to test the pyWarpUstruct module
+from mpi4py import MPI
+from pywarpustruct import USMesh
+
+options = {
+    'fileType':'CGNS',
+    'gridFile':'../../input_files/o_mesh.cgns',
+    'aExp': 3.0,
+    'bExp': 5.0,
+    'LdefFact':100.0,
+    'alpha':0.25,
+    'errTol':0.0001,
+    'evalMode':'fast',
+    'symmTol':1e-6,
+    'useRotations':True,
+    'bucketSize':8,
+}
+
+# Create the mesh object
+mesh = USMesh(options=options, comm=MPI.COMM_WORLD)
+
+coords0 = mesh.getSurfaceCoordinates()
+
+new_coords = coords0.copy()
+for i in xrange(len(coords0)):
+    new_coords[i, :] *= 1.1
+
+# Reset the newly computed surface coordiantes
+mesh.setSurfaceCoordinates(new_coords)
+
+# Actually run the mesh warping
+mesh.warpMesh()
+
+# Write the new grid file.
+mesh.writeGrid('warped.cgns')
+
+import numpy as np
+
+np.random.seed(314)
+dXsd = np.random.random((1439, 3))
+dXvWarpd = mesh.warpDerivFwd(dXsd, solverVec=False)
+
+dXvWarpb = np.random.random(mesh.warp.griddata.warpmeshdof)
+mesh.warpDeriv(dXvWarpb, solverVec=False)
+dXsb = mesh.getdXs()
+
+dotProd = 0.0
+dotProd += np.sum(dXvWarpd * dXvWarpb)
+dotProd -= np.sum(dXsb * dXsd)
+print 'dotProd should be zero:', dotProd
+print
+
+print mesh.getdXs()
