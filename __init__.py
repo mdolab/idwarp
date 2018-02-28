a38281aa6b8c19a1e1afaf1a062ecb43ff8422a2
Ney Secco <neysecco@users.noreply.github.com>
2018-02-28 16:36:49 -0500

    Added complex version of pyWarpMulti.

--- __init__.py
+++ __init__.py
@@ -1,3 +1,4 @@
 from .python.UnstructuredMesh import USMesh
 from .python.MultiUnstructuredMesh import MultiUSMesh
 from .python.UnstructuredMesh_C import USMesh_C
+from .python.MultiUnstructuredMesh_C import MultiUSMesh_C
