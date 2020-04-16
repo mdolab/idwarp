820e83272c9c77fc15abd99cfab1bbadf2ffa54d
Neil Wu <neilwu0626@gmail.com>
2020-04-16 11:55:57 -0400

    more py3 syntax fixes

--- src/f2py/importTest.py
+++ src/f2py/importTest.py
@@ -2,12 +2,12 @@
 import sys
 
 name = 'idwarp'
-print ("Testing if module %s can be imported..." % name)
+print("Testing if module %s can be imported..." % name)
 import_cmd = "import %s" % name
 try:
     exec(import_cmd)
 except:
-    print ("Error: idwarp was not imported correctly")
+    print("Error: idwarp was not imported correctly")
     sys.exit(1)
 # end try
 
