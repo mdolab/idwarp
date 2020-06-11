398774719de11727a8310d6762d75560d3f06fe8
joanibal <joanibal@umich.edu>
2020-06-11 18:09:50 -0400

    use setup.py

---
+++ idwarp/__init__.py
@@ -0,0 +1,6 @@
+__version__ = '2.1.0'
+
+from .UnstructuredMesh import USMesh
+from .MultiUnstructuredMesh import MultiUSMesh
+from .UnstructuredMesh_C import USMesh_C
+from .MultiUnstructuredMesh_C import MultiUSMesh_C
