bf4dee6208a33c55b6380fff4d73b6feb3618950
Neil Wu <neilwu@umich.edu>
2020-07-27 14:29:10 -0400

    updated links to use AFS

--- input_files/get-input-files.sh
+++ input_files/get-input-files.sh
@@ -2,5 +2,5 @@
 # this file will download the input files for IDWarp regression tests
 # and extract them to the right place.
 
-wget -O input_files.tar.gz https://umich.box.com/shared/static/rrr6zruxhzbx9eba7wcfzdwmdn0fq52k
-tar -xf input_files.tar.gz -C ../
+wget http://umich.edu/~mdolaboratory/repo_files/IDWarp/input_files.tar.gz
+tar -xzf input_files.tar.gz -C ../
