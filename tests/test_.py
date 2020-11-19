3d6efb74d3682026b9b10e73485be84cdd891988
Marco Mangano <mmangano@umich.edu>
2020-11-19 11:05:14 +0100

    initial setupand test2 migration

---
+++ tests/test_.py
@@ -0,0 +1,99 @@
+# =============================================================================
+# Standard Python modules
+# =============================================================================
+import os
+import copy
+
+# =============================================================================
+# External Python modules
+# =============================================================================
+import numpy
+from numpy.testing import assert_allclose
+import unittest
+from mpi4py import MPI
+
+# =============================================================================
+# Extension modules
+# =============================================================================
+from idwarp import USMesh
+from baseclasses import BaseRegTest
+
+baseDir = os.path.dirname(os.path.abspath(__file__))
+
+
+class Test(unittest.TestCase):
+
+    N_PROCS = 1
+
+    def setUp(self):
+        self.ref_file = os.path.join(baseDir, "ref/test_.ref")
+
+        self.defOpts = {
+            "gridFile": None,
+            "aExp": 3.0,
+            "bExp": 5.0,
+            "LdefFact": 1.0,
+            "alpha": 0.25,
+            "errTol": 0.0005,
+            "evalMode": "fast",
+            "symmTol": 1e-6,
+            "useRotations": True,
+            "bucketSize": 8,
+            "fileType": None,
+        }
+
+    def train_comesh(self, train=True):
+        self.test_comesh(train=train)
+        # handler.writeRef()
+
+    def test_comesh(self, train=False):
+        with BaseRegTest(self.ref_file, train=train) as handler:
+            test_name = "Test_co_mesh"
+            file_name = os.path.join(baseDir, "../input_files/co_mesh.cgns")
+
+            meshOptions = copy.deepcopy(self.defOpts)
+
+            meshOptions.update({"gridFile": file_name, "fileType": "cgns"})
+            # Create warping object
+            mesh = USMesh(options=meshOptions)
+
+            # Extract Surface Coordinates
+            coords0 = mesh.getSurfaceCoordinates()
+
+            vCoords = mesh.getCommonGrid()
+            val = MPI.COMM_WORLD.reduce(numpy.sum(vCoords.flatten()), op=MPI.SUM)
+            handler.root_add_val(f"{test_name} - Sum of vCoords Inital:", val, tol=1e-8)
+
+            new_coords = coords0.copy()
+            # Do a shearing sweep deflection:
+            for i in range(len(coords0)):
+                span = coords0[i, 2]
+                new_coords[i, 0] += 0.05 * span
+
+            # Reset the newly computed surface coordiantes
+            mesh.setSurfaceCoordinates(new_coords)
+            mesh.warpMesh()
+
+            # Get the sum of the warped coordinates
+            # vCoords = mesh.getSolverGrid()
+            vCoords = mesh.getWarpGrid()
+
+            val = MPI.COMM_WORLD.reduce(numpy.sum(vCoords.flatten()), op=MPI.SUM)
+            handler.root_add_val(f"{test_name} - Sum of vCoords Warped:", val, tol=1e-8)
+
+            # Create a dXv vector to do test the mesh warping with:
+            dXv_warp = numpy.linspace(0, 1.0, mesh.warp.griddata.warpmeshdof)
+            # if MPI.COMM_WORLD.rank == 0:
+            #     print("Computing Warp Deriv")
+            mesh.warpDeriv(dXv_warp, solverVec=False)
+            dXs = mesh.getdXs()
+
+            val = MPI.COMM_WORLD.reduce(numpy.sum(dXs.flatten()), op=MPI.SUM)
+            handler.root_add_val(f"{test_name} - Sum of dxs:", val, tol=1e-8)
+
+            # if MPI.COMM_WORLD.rank == 0:
+            #     print("Verifying Warp Deriv")
+            mesh.verifyWarpDeriv(dXv_warp, solverVec=False, dofStart=0, dofEnd=5)
+
+            if train is True:
+                handler.writeRef()
