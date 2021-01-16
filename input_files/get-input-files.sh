0c50685b15770780b9194f8837e39f483cec9024
Neil Wu <neilwu@umich.edu>
2021-01-16 12:19:32 -0500

    [ci skip] updated bash script

--- input_files/get-input-files.sh
+++ input_files/get-input-files.sh
@@ -2,5 +2,6 @@
 # this file will download the input files for IDWarp regression tests
 # and extract them to the right place.
 
-wget http://umich.edu/~mdolaboratory/repo_files/IDWarp/input_files.tar.gz
-tar -xzf input_files.tar.gz -C ../
+DIR=$(dirname $0)
+wget -O $DIR/input_files.tar.gz http://umich.edu/~mdolaboratory/repo_files/IDWarp/input_files.tar.gz 
+tar -xzf $DIR/input_files.tar.gz -C $DIR/../
