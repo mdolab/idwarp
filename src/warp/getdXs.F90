f72fe45207b840c66afc747c96901934dad0c4ec
Alasdair Gray <alachris@umich.edu>
2021-07-14 18:14:49 -0400

    Support latest PETSC (#54)

--- src/warp/getdXs.F90
+++ src/warp/getdXs.F90
@@ -2,22 +2,20 @@ subroutine getdXs(output, ndof)
 
   use gridData
   implicit none
-  
+
   ! Input Arguments
   integer(kind=intType) ,  intent(in) :: ndof
-  
+
   ! Output Arguments
   real(kind=realType)   ,  intent(inout) :: output(ndof)
-  
+
   ! Local Arguments
   integer(kind=intType) :: ierr, istart, iend, i
-  
+
   call VecGetOwnershipRange(dXs, istart, iend, ierr)
   call EChk(ierr, __FILE__, __LINE__)
 
-  do i=1, ndof
-     call VecGetValues(dXs, 1,  (/i+istart-1/), output(i), ierr)
-     call EChk(ierr, __FILE__, __LINE__)
-  end do
+  call VecGetValues(dXs, ndof, (/(i, i=istart,iend,1)/), output(1:ndof), ierr)
+  call EChk(ierr, __FILE__, __LINE__)
 
 end subroutine getdXs
