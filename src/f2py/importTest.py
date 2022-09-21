e1c04f8b842a594cfb8488dd469472d31bd1bc79
Eirikur Jonsson <eirikurj@umich.edu>
2022-09-21 18:20:39 +0000

    Update library import checks

--- src/f2py/importTest.py
+++ src/f2py/importTest.py
@@ -1,14 +1,22 @@
-#! /usr/bin/env python
+#!/usr/bin/env python
 import sys
+import os
+import argparse
 
-name = "libidwarp"
-print("Testing if module %s can be imported..." % name)
-import_cmd = "import %s" % name
+parser = argparse.ArgumentParser()
+parser.add_argument("name", type=str, help="Library name (example: libpackage.so). Note: This script must be run in the same dir as the library.")
+args = parser.parse_args()
+
+# Only get the filename without the extension
+name = os.path.splitext(args.name)[0]
+print(f"Testing if module {name} can be imported...")
+
 try:
+    import_cmd = f"import {name}"
     exec(import_cmd)
-except ImportError:
-    print("Error: idwarp was not imported correctly")
+except ImportError as e:
+    print(f"Error: {e}")
+    print(f"Error: library {args.name} was not imported correctly")
     sys.exit(1)
-# end try
 
-print("Module %s was successfully imported." % name)
+print(f"Module {name} was successfully imported")
