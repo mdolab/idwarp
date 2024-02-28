3d169ddcd4bfce860ee057b9f3c52205969dc1dd
Andrew Lamkin <lamkina@umich.edu>
2024-02-27 20:37:28 -0500

    Reverted back to only reading on the root proc

--- src/IO/readCGNS.F90
+++ src/IO/readCGNS.F90
@@ -43,46 +43,46 @@ subroutine readCGNS(cgns_file)
 
     ! Do the I/O that is common to both types of grids
 
-    ! if (myid == 0) then
-    print *, ' -> Reading CGNS File: ', cgns_file
+    if (myid == 0) then
+        print *, ' -> Reading CGNS File: ', cgns_file
 
-    ! Open and get the number of zones:
-    call cg_open_f(trim(cgns_file), CG_MODE_READ, cg, ierr)
-    if (ierr .eq. CG_ERROR) call cg_error_exit_f
+        ! Open and get the number of zones:
+        call cg_open_f(trim(cgns_file), CG_MODE_READ, cg, ierr)
+        if (ierr .eq. CG_ERROR) call cg_error_exit_f
 
-    call cg_nbases_f(cg, nbases, ierr)
-    if (ierr .eq. CG_ERROR) call cg_error_exit_f
+        call cg_nbases_f(cg, nbases, ierr)
+        if (ierr .eq. CG_ERROR) call cg_error_exit_f
 
-    if (nbases .gt. 1) then
-        print *, ' ** Warning: IDWarp only reads the first base in a cgns file'
-    end if
-
-    base = 1_intType
-
-    call cg_base_read_f(cg, base, basename, CellDim, PhysDim, ierr)
-    if (ierr .eq. CG_ERROR) call cg_error_exit_f
-    if (cellDim .ne. 3 .or. PhysDim .ne. 3) then
-        print *, 'The Cells must 3 dimensional'
-        stop
-    end if
-
-    call cg_nzones_f(cg, base, nZones, ierr); 
-    if (ierr .eq. CG_ERROR) call cg_error_exit_f
-    print *, '   -> Number of Zones:', nzones
-
-    ! Determine if we have structured or unstructured zones. We can
-    ! only deal with one or the other.
-    nStructured = 0
-    nUnstructured = 0
-    do i = 1, nZones
-        call cg_zone_type_f(cg, base, i, zoneType, ierr)
-        if (zoneType == Structured) then
-            nStructured = nStructured + 1
-        else if (zoneType == Unstructured) then
-            nUnstructured = nUnstructured + 1
+        if (nbases .gt. 1) then
+            print *, ' ** Warning: IDWarp only reads the first base in a cgns file'
         end if
-    end do
-    ! end if
+
+        base = 1_intType
+
+        call cg_base_read_f(cg, base, basename, CellDim, PhysDim, ierr)
+        if (ierr .eq. CG_ERROR) call cg_error_exit_f
+        if (cellDim .ne. 3 .or. PhysDim .ne. 3) then
+            print *, 'The Cells must 3 dimensional'
+            stop
+        end if
+
+        call cg_nzones_f(cg, base, nZones, ierr); 
+        if (ierr .eq. CG_ERROR) call cg_error_exit_f
+        print *, '   -> Number of Zones:', nzones
+
+        ! Determine if we have structured or unstructured zones. We can
+        ! only deal with one or the other.
+        nStructured = 0
+        nUnstructured = 0
+        do i = 1, nZones
+            call cg_zone_type_f(cg, base, i, zoneType, ierr)
+            if (zoneType == Structured) then
+                nStructured = nStructured + 1
+            else if (zoneType == Unstructured) then
+                nUnstructured = nUnstructured + 1
+            end if
+        end do
+    end if
 
     ! Broadcast the results to everyone
     call MPI_bcast(nStructured, 1, MPI_INTEGER, 0, warp_comm_world, ierr)
