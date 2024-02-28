2287f7077a1ab167ca052f1a70a46d5ae191ddb2
Andrew Lamkin <lamkina@umich.edu>
2024-02-27 20:38:25 -0500

    Added the axisymmetric mesh class

--- idwarp/__init__.py
+++ idwarp/__init__.py
@@ -1,8 +1,9 @@
 __version__ = "2.6.1"
 
+from .AxisymmetricMesh import AxisymmetricMesh
 from .UnstructuredMesh import USMesh
 from .UnstructuredMesh_C import USMesh_C
 from .MultiUnstructuredMesh import MultiUSMesh
 from .MultiUnstructuredMesh_C import MultiUSMesh_C
 
-__all__ = ["USMesh", "MultiUSMesh", "USMesh_C", "MultiUSMesh_C"]
+__all__ = ["AxisymmetricMesh", "USMesh", "MultiUSMesh", "USMesh_C", "MultiUSMesh_C"]
